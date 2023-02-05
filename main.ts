bb0590e5b8ca89d18e8d7b385e9b5def14b68fe3
DGNSHN <33982400+DgNShN@users.noreply.github.com>
2023-02-05 17:33:48 +0300

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,12 @@
 input.onButtonPressed(Button.A, function () {
     Aoyuncu += 1
     led.plotBrightness(0, Aoyuncu - 1, Aoyuncu * 51)
+    Boyuncu = 0
+})
+input.onButtonPressed(Button.AB, function () {
+    Aoyuncu = 0
+    Boyuncu = 0
+    basic.clearScreen()
 })
 input.onButtonPressed(Button.B, function () {
     Boyuncu += 1
